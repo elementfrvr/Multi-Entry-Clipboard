5ea93167ee2f06b2a60e8cbe0f41db3c9e3a2750
Dakota Mitchem <mitchemdakota@gmail.com>
2022-07-23 15:42:23 -0500

    Added includes and filepath

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,12 @@
 // Multiple entry clipboard
-//
 #include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <string>
+using std::string;
 
-int main( int argc, char *argv[] )
-{
+string FilePath = "C:\\Users\\koda1\\Desktop\\Payload.txt";
+
+int main( int argc, char *argv[] ){
 
 }
