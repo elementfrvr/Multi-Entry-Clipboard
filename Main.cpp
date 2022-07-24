c0455da8618722345220aba1b2e91f159402208c
Dakota Mitchem <mitchemdakota@gmail.com>
2022-07-23 19:06:27 -0500

    Added IO and skeleton code

--- Main.cpp
+++ Main.cpp
@@ -4,11 +4,31 @@
 #include <iostream>
 #include <string>
 #include <vector>
-using std::vector;
-using std::string;
+using namespace std;
 
-string FilePath = "C:\\Users\\koda1\\Desktop\\Payload.txt";
+ const string FilePath = "C:/Users/koda1/Desktop/Payload.txt";
 
 int main( int argc, char *argv[] ){
+    char data[100];
+    ifstream fileIn;
+    fileIn.open(FilePath);
+   fileIn >> data; 
 
+   // write the data at the screen.
+   cout << data << endl;
+
+
+}
+
+string Stringbuilder(string currItem){
+    //Take each line and place it into a single comma separated string to populate vector
+    string fake = currItem;
+    return fake;
+}
+
+vector<string> FillVector(string vectorList){
+    // fill vector using string from string builder
+    //vector<string> clipboard = {null, null, null};
+    vector<string> test2 = {"a", "b"};
+    return test2;
 }
