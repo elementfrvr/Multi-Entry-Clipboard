9896d3b1c289e8c55e4efa76959f02a0f875e7cd
Dakota Mitchem <mitchemdakota@gmail.com>
2022-07-23 19:50:28 -0500

    Add string builder code

--- Main.cpp
+++ Main.cpp
@@ -6,24 +6,16 @@
 #include <vector>
 using namespace std;
 
- const string FilePath = "C:/Users/koda1/Desktop/Payload.txt";
-
-int main( int argc, char *argv[] ){
-    char data[100];
-    ifstream fileIn;
-    fileIn.open(FilePath);
-   fileIn >> data; 
-
-   // write the data at the screen.
-   cout << data << endl;
+const string FilePath = "C:/Users/koda1/Desktop/Payload.txt";
 
 
-}
 
-string Stringbuilder(string currItem){
+ string MakeString(string currItem, string list){
     //Take each line and place it into a single comma separated string to populate vector
-    string fake = currItem;
-    return fake;
+    string vectorList;
+    vectorList = list + currItem + ", ";
+    
+    return vectorList;
 }
 
 vector<string> FillVector(string vectorList){
@@ -31,4 +23,26 @@ vector<string> FillVector(string vectorList){
     //vector<string> clipboard = {null, null, null};
     vector<string> test2 = {"a", "b"};
     return test2;
+}
+
+int main( int argc, char *argv[] ){
+    int count = 1;
+    char data[100];
+    string currList;
+    string currItem;
+    ifstream fileIn;
+    fileIn.open(FilePath);
+    if(!fileIn) 
+        {
+            cout<<"Error opening output file"<<endl;
+            system("pause");
+            return -1;
+        }
+    while(!fileIn.eof()){
+        getline(fileIn, currItem);
+        currList = MakeString(currItem, currList);
+    }
+    
+
+
 }
